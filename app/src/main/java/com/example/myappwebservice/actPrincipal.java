111f02343f924fc589a7e9ab4e9e7fb37885f1ce
Henry Pérez <henry.perez2017@uteq.edu.ec>
2020-11-27 14:37:42 -0500

    Corrección del token adquirido por Kushki

--- app/src/main/java/com/example/myappwebservice/actPrincipal.java
+++ app/src/main/java/com/example/myappwebservice/actPrincipal.java
@@ -1,15 +1,21 @@
 package com.example.myappwebservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.myappwebservice.WebService.Asynchtask;
 import com.example.myappwebservice.WebService.WebService;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,17 +28,30 @@ public class actPrincipal extends AppCompatActivity implements Asynchtask {
 
         Bundle bundle = this.getIntent().getExtras();
 
-        Map<String, String> datos = new HashMap<String, String>();
+        Map<String, String> datos = new HashMap<>();
 
-        WebService webService = new WebService("https://api-uat.kushkipagos.com/transfer/v1/bankList/Public-Merchant-Id="+bundle.getString("token"),
+        WebService webService = new WebService("https://api-uat.kushkipagos.com/transfer/v1/bankList",
                 datos, actPrincipal.this, actPrincipal.this);
 
-        webService.execute("GET");
+        webService.execute("GET", "Public-Merchant-Id", bundle.getString("token"));
     }
 
     @Override
-    public void processFinish(String result) throws JSONException {
+    public void processFinish(String result) {
         TextView respueta = findViewById(R.id.txtMensaje);
-        respueta.setText(result);
+        ArrayList<String> lsBancos = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+            for (int i = 0; i < jsonArray.length(); i++)
+            {
+                JSONObject banco = jsonArray.getJSONObject(i);
+                lsBancos.add(banco.getString("name"));
+            }
+            respueta.setText("El token que ingreso es valido");
+            RecyclerView rc = findViewById(R.id.rcView);
+        }catch (Exception e)
+        {
+            respueta.setText("El token que ingreso no es valido" );
+        }
     }
 }
